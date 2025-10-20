b6fd2a28986f3c609f10609b6c850fa9fbbe26c1
Pui Yong Qing <puiyongqing@gmail.com>
2025-10-20 10:14:57 +0800

    ollama: add open-webui

--- modules/core/ollama.nix
+++ modules/core/ollama.nix
@@ -1,10 +1,13 @@
 { pkgs, ... }:
 {
-  services.ollama = {
-    enable = true;
-    package = pkgs.ollama-rocm;
-    acceleration = "rocm";
-    rocmOverrideGfx = "11.0.0";
+  services = {
+    open-webui.enable = true;
+    ollama = {
+      enable = true;
+      package = pkgs.ollama-rocm;
+      acceleration = "rocm";
+      rocmOverrideGfx = "11.0.0";
+    };
   };
   boot.kernelModules = [ "amdgpu" ]; # workaround of race condition (see issue #422355 on nixpkgs)
 }
